ceb1b554744e619e79b6a266d46f56b60ef80f60
Jules Fakhouri <jules.fakhouri@qobuz.com>
2024-07-27 23:12:15 +0200

    Update Tailwind CSS configuration with new fonts and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        "caveat": ["var(--font-caveat)"],
+            },
+      colors: {
+        primary: {
+          '50': '#f3faf7',
+          '100': '#d6f1e6',
+          '200': '#aee1cd',
+          '300': '#76c8ab', // Couleur principale
+          '400': '#52af91',
+          '500': '#389478',
+          '600': '#2b7660',
+          '700': '#265f50',
+          '800': '#224d42',
+          '900': '#204139',
+          '950': '#0e2520',
+        },
       },
     },
   },
   plugins: [],
 };
+
 export default config;
