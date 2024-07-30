6f805ef1337a248c0d5b5a9ec0325d926db97fdd
Jules Fakhouri <jules.fakhouri@qobuz.com>
2024-07-30 14:48:32 +0200

    add image domain configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// @ts-check
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['lh3.googleusercontent.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
--- src/app/api/test/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  console.log('Test API route hit');
-  return NextResponse.json({ message: "API is working" });
-}
